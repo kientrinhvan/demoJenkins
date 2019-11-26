5030e52efd112756d1d4e89fc534d069d07bb76a
Phat Ngo <phat.ngo@logigear.com>
2019-11-26 17:03:33 +0700

    phat

--- kientrinhsort.java
+++ kientrinhsort.java
@@ -26,7 +26,7 @@ public class kientrinhsort {
 		
 		// output
 		System.out.println(Arrays.toString(intArray));
-		System.out.println("what");
+		System.out.println("whataaaa");
     }
 	
 	private static void mySort(int[] intArray){
